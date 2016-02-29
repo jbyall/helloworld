8fec7638756032f703184da1793dc8399250b8ac
jbyall <jpb5002@gmail.com>
2016-02-28 23:07:46 -0500

    added users

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
-server 'ec2-52-86-109-24.compute-1.amazonaws.com', user: 'deploy', roles: %w{web app}
+server 'ec2-52-86-109-24.compute-1.amazonaws.com', user: 'deploy', roles: %w{web app db}
 
 # server-based syntax
 # ======================
