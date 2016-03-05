026ae7a7838cc4a8d521c7897fe295423129fe9a
jbyall <jpb5002@gmail.com>
2016-03-05 14:45:23 -0500

    added resume static site

---
+++ public/skeleton.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    $('#slider').flexslider({
+        animation: "slide",
+        slideshow: false,
+        directionNav: false,
+        manualControls: ".menu li"
+    });
+
+    // $('#menu-item1').addClass("menu-selected");
+    // $('#menu-item2').addClass("menu-selected");
+    // $('#menu-item3').addClass("menu-selected");
+
+    $('#menu-item1').addClass("menu-selected");
+    $('#menu-item2').addClass("menu-not-selected");
+    $('#menu-item3').addClass("menu-not-selected");
+
+    $('#menu-item1').click(function(){
+        $('#menu-item1').removeClass("menu-not-selected");
+        $('#menu-item1').addClass("menu-selected");
+
+        $('#menu-item2').removeClass("menu-selected");
+        $('#menu-item2').addClass("menu-not-selected");
+
+        $('#menu-item3').removeClass("menu-selected");
+        $('#menu-item3').addClass("menu-not-selected");
+    });
+
+    $('#menu-item2').click(function(){
+        $('#menu-item1').removeClass("menu-selected");
+        $('#menu-item1').addClass("menu-not-selected");
+
+        $('#menu-item2').removeClass("menu-not-selected");
+        $('#menu-item2').addClass("menu-selected");
+
+        $('#menu-item3').removeClass("menu-selected");
+        $('#menu-item3').addClass("menu-not-selected");
+    });
+
+    $('#menu-item3').click(function(){
+        $('#menu-item1').removeClass("menu-selected");
+        $('#menu-item1').addClass("menu-not-selected");
+
+        $('#menu-item2').removeClass("menu-selected");
+        $('#menu-item2').addClass("menu-not-selected");
+
+        $('#menu-item3').removeClass("menu-not-selected");
+        $('#menu-item3').addClass("menu-selected");
+    });
+});
