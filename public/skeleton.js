672338eeed6890ae04c912c05e2808af58a347ca
jbyall <jpb5002@gmail.com>
2016-03-05 14:53:46 -0500

    updated skeleton.js

--- public/skeleton.js
+++ public/skeleton.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     $('#menu-item2').addClass("menu-not-selected");
     $('#menu-item3').addClass("menu-not-selected");
 
-    $('#menu-item1').click(function(){
+    $('#menu-item1').on('click touchstart', (function(){
         $('#menu-item1').removeClass("menu-not-selected");
         $('#menu-item1').addClass("menu-selected");
 
@@ -25,7 +25,7 @@ $(document).ready(function() {
         $('#menu-item3').addClass("menu-not-selected");
     });
 
-    $('#menu-item2').click(function(){
+    $('#menu-item2').on('click touchstart', (function(){
         $('#menu-item1').removeClass("menu-selected");
         $('#menu-item1').addClass("menu-not-selected");
 
@@ -36,7 +36,7 @@ $(document).ready(function() {
         $('#menu-item3').addClass("menu-not-selected");
     });
 
-    $('#menu-item3').click(function(){
+    $('#menu-item3').on('click touchstart', (function(){
         $('#menu-item1').removeClass("menu-selected");
         $('#menu-item1').addClass("menu-not-selected");
 
