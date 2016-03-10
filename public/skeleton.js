d369fd39ab4129392de66c4586ddaf854a9c0b29
jbyall <jpb5002@gmail.com>
2016-03-10 10:27:02 -0500

    more updates

--- public/skeleton.js
+++ public/skeleton.js
@@ -15,46 +15,46 @@ $(document).ready(function() {
     $('#menu-item3 a').addClass("menu-not-selected");
 
     $('#menu-item1 a').on('click touch', function(){
-        // $('#menu-item1 a').removeClass("menu-not-selected");
-        // $('#menu-item1 a').addClass("menu-selected");
+        $('#menu-item1 a').removeClass("menu-not-selected");
+        $('#menu-item1 a').addClass("menu-selected");
         
 
-        // $('#menu-item2 a').removeClass("menu-selected");
-        // $('#menu-item2 a').addClass("menu-not-selected");
+        $('#menu-item2 a').removeClass("menu-selected");
+        $('#menu-item2 a').addClass("menu-not-selected");
         
 
-        // $('#menu-item3 a').removeClass("menu-selected");
-        // $('#menu-item2 a').addClass("menu-not-selected");
-        $('#menu-item1 a').toggleClass('menu-not-selected menu-selected');
-        $('#menu-item2 a').toggleClass('menu-selected menu-not-selected');
-        $('#menu-item3 a').toggleClass('menu-selected menu-not-selected');
+        $('#menu-item3 a').removeClass("menu-selected");
+        $('#menu-item2 a').addClass("menu-not-selected");
+        // $('#menu-item1 a').toggleClass('menu-not-selected menu-selected');
+        // $('#menu-item2 a').toggleClass('menu-selected menu-not-selected');
+        // $('#menu-item3 a').toggleClass('menu-selected menu-not-selected');
     });
 
     $('#menu-item2 a').on('click touch', function(){
-        // $('#menu-item1 a').removeClass("menu-selected");
-        // $('#menu-item1 a').addClass("menu-not-selected");
+        $('#menu-item1 a').removeClass("menu-selected");
+        $('#menu-item1 a').addClass("menu-not-selected");
 
-        // $('#menu-item2 a').removeClass("menu-not-selected");
-        // $('#menu-item2 a').addClass("menu-selected");
+        $('#menu-item2 a').removeClass("menu-not-selected");
+        $('#menu-item2 a').addClass("menu-selected");
 
-        // $('#menu-item3 a').removeClass("menu-selected");
-        // $('#menu-item3 a').addClass("menu-not-selected");
-        $('#menu-item1 a').toggleClass('menu-selected menu-not-selected');
-        $('#menu-item2 a').toggleClass('menu-not-selected menu-selected');
-        $('#menu-item3 a').toggleClass('menu-selected menu-not-selected');
+        $('#menu-item3 a').removeClass("menu-selected");
+        $('#menu-item3 a').addClass("menu-not-selected");
+        // $('#menu-item1 a').toggleClass('menu-selected menu-not-selected');
+        // $('#menu-item2 a').toggleClass('menu-not-selected menu-selected');
+        // $('#menu-item3 a').toggleClass('menu-selected menu-not-selected');
     });
 
     $('#menu-item3 a').on('click touch', function(){
-        // $('#menu-item1 a').removeClass("menu-selected");
-        // $('#menu-item1 a').addClass("menu-not-selected");
+        $('#menu-item1 a').removeClass("menu-selected");
+        $('#menu-item1 a').addClass("menu-not-selected");
 
-        // $('#menu-item2 a').removeClass("menu-selected");
-        // $('#menu-item2 a').addClass("menu-not-selected");
+        $('#menu-item2 a').removeClass("menu-selected");
+        $('#menu-item2 a').addClass("menu-not-selected");
 
-        // $('#menu-item3 a').removeClass("menu-not-selected");
-        // $('#menu-item3 a').addClass("menu-selected");
-        $('#menu-item1 a').toggleClass('menu-selected menu-not-selected');
-        $('#menu-item2 a').toggleClass('menu-selected menu-not-selected');
-        $('#menu-item3 a').toggleClass('menu-not-selected menu-selected');
+        $('#menu-item3 a').removeClass("menu-not-selected");
+        $('#menu-item3 a').addClass("menu-selected");
+        // $('#menu-item1 a').toggleClass('menu-selected menu-not-selected');
+        // $('#menu-item2 a').toggleClass('menu-selected menu-not-selected');
+        // $('#menu-item3 a').toggleClass('menu-not-selected menu-selected');
     });
 });
